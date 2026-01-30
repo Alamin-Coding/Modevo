3239e329c98a353deaf3529a348f14d5ffeba9f5
Alamin-Coding <md.alamin.coding@gmail.com>
2026-01-30 21:52:31 +0600

    footer add

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
+import Footer from "./footer/Footer";
 
 const MainLayout = () => {
 	return (
 		<>
 			<Header />
 			<Outlet />
-			<footer>Footer</footer>
+			<Footer />
 		</>
 	);
 };
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer>
+			<div className="container">
+				<div className="grid grid-cols-4">
+					<div>1</div>
+					<div>2</div>
+					<div>3</div>
+					<div>4</div>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;

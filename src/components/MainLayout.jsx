8d424314f390c186d5c81bf6477e09c44b7fbcc4
taha-rat <taharat.sabikun@gmail.com>
2026-01-30 23:57:49 +0600

    Blogs add

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
+import Blogs from "./Blogs/Blogs"
 
 const MainLayout = () => {
 	return (
 		<>
 			<Header />
 			<Outlet />
+			<Blogs />
 			<footer>Footer</footer>
+			
+			
 		</>
 	);
 };

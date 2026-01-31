2853cc7f580ad2dd6167e494e6e19ff7a4122b4f
Jannat <jannatislam748926@gmail.com>
2026-01-31 16:50:34 +0600

    Exlusive add

---
+++ src/components/Exlusive part/Exlusive.jsx
@@ -0,0 +1,15 @@
+import "./Exclusive.css";
+
+function Exclusive() {
+  return (
+    <div className="exclusive-container">
+      <p className="exclusive-text">
+        Exlusive Member - plus 8% reward and free shipping
+      </p>
+
+      <button className="shop-btn">SHOP NOW</button>
+    </div>
+  );
+}
+
+export default Exclusive;

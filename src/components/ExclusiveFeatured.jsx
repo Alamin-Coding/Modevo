e0c4e2b997ba62f5c524187b674916d7e6e01585
Alamin-Coding <alamin.codelover@gmail.com>
2026-02-01 01:00:47 +0600

    all barnch marged

--- src/components/ExclusiveFeatured.jsx
+++ src/components/ExclusiveFeatured.jsx
@@ -8,81 +8,74 @@ import ExclusiveFeaturedCard from "./ExclusiveFeaturedCard";
 
 import left from "../assets/left.png";
 import right from "../assets/right.png";
-import fotter from "../assets/exclusive.png";
 // card images
 import exclusive_1 from "../assets/exclusive-1.png";
 import exclusive_2 from "../assets/exclusive-2.png";
 import exclusive_3 from "../assets/exclusive-3.png";
 import exclusive_4 from "../assets/exclusive-4.png";
 
-import color from "../assets/color.png";
-import color_1 from "../assets/color-1.png";
-
 // exclusive featured component
 
 const ExclusiveFeatured = () => {
-  return (
-    // section start
-    <section>
-      <div className="container py-12 relative">
+	return (
+		// section start
+		<section>
+			<div className="container py-12 relative">
+				{/* card header */}
 
-        {/* card header */}
+				<h2 className="flex items-center justify-center font-medium text-[40px] ">
+					Featured
+				</h2>
 
-        <h2 className="flex items-center justify-center font-medium text-[40px] ">
-          Featured
-        </h2>
+				{/* card design */}
+				<div className="grid grid-cols-4 justify-between gap-8 py-15">
+					<ExclusiveFeaturedCard
+						title="Relaxed Stitch Shirt"
+						img={exclusive_1}
+						text="ADD TO CART"
+						showUnderline={true}
+						colors={["#55483B", "#403D72", "#E5C2C3", "#E6DDD6"]}
+					/>
+					<ExclusiveFeaturedCard
+						title="Off-Duty Denim Set"
+						img={exclusive_2}
+						text="ADD TO CART"
+						showUnderline={true}
+						colors={["#3A582C", "#2B2A2F", "#3A582C"]}
+					/>
+					<ExclusiveFeaturedCard
+						title="Off-Duty Denim Set"
+						img={exclusive_3}
+						text="ADD TO CART"
+						showUnderline={true}
+						colors={["#3A582C", "#2B2A2F", "#3A582C"]}
+					/>
+					<ExclusiveFeaturedCard
+						title="Off-Duty Denim Set"
+						img={exclusive_4}
+						text="ADD TO CART"
+						showUnderline={true}
+						colors={["#3A582C", "#2B2A2F", "#3A582C"]}
+					/>
+				</div>
 
-        {/* card design */}
+				{/* card slide button */}
 
-        <div className="grid grid-cols-4 justify-between gap-8 py-15">
-          <ExclusiveFeaturedCard
-            title="Relaxed Stitch Shirt"
-            img={exclusive_1}
-            color={color}
-            text="ADD TO CART"
-            showUnderline={true}
-          />
-          <ExclusiveFeaturedCard
-            title="Off-Duty Denim Set"
-            img={exclusive_3}
-            color={color_1}
-            text="$179.000"
-          />
-          <ExclusiveFeaturedCard
-            title="Utility Pocket Tee"
-            img={exclusive_2}
-            color={color_1}
-            text="$278.000"
-          />
-          <ExclusiveFeaturedCard
-            title="Retro Crop Tank & Cargo"
-            img={exclusive_4}
-            color={color_1}
-            text="$223.000"
-          />
-        </div>
+				<span className="absolute left-[-2.5%] top-[45%] transform -translate-y-1/2">
+					<Button>
+						<img src={left} alt="Left" />
+					</Button>
+				</span>
 
-        {/* card slide button */}
-
-        <span className="absolute left-[-2.5%] top-[45%] transform -translate-y-1/2">
-          <Button>
-            <img src={left} alt="Left" />
-          </Button>
-        </span>
-
-        <span className="absolute right-[-2.5%] top-[45%] transform -translate-y-1/2">
-          <Button>
-            <img src={right} alt="Right" />
-          </Button>
-        </span>
-        {/* card slide icon */}
-        <div className="flex justify-center items-center">
-            <img src={fotter} alt="slide" />
-        </div>
-      </div>
-    </section>
-    // section end
-  );
+				<span className="absolute right-[-2.5%] top-[45%] transform -translate-y-1/2">
+					<Button>
+						<img src={right} alt="Right" />
+					</Button>
+				</span>
+			</div>
+		</section>
+		// section end
+	);
 };
 
 export default ExclusiveFeatured;

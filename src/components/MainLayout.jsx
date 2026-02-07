d9a48157226bbfcc9b8d4585a98c8d387b85ac11
Alamin-Coding <alamin.codelover@gmail.com>
2026-02-07 21:01:04 +0600

    footer section add

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
+import Footer from "./footer";
 
 const MainLayout = () => {
 	return (
 		<>
 			<Header />
 			<Outlet />
-			<footer>Footer</footer>
+			<Footer />
 		</>
 	);
 };
